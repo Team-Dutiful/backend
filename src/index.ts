55dbc5b6a8615c10ba01e3230b7a53750558db20
yh <azss0470@gmail.com>
2022-10-18 23:16:52 +0900

    chore: sequelize-mysql db연동 완료

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { sequelize } from "../models";
+
+dotenv.config();
+
+const PORT: number = parseInt(process.env.DB_PORT as string, 10) || 5000;
+const HOST: string = process.env.DB_HOST || "localhost";
+const DATEBASE: string = process.env.DB_DBNAME;
 
 const app: Express = express();
-const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`Request Occur! ${req.method}, ${req.url}`);
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Dutiful");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at port-${port}`);
+app.listen(PORT, HOST, async () => {
+  console.log(`[server]: Server is running at ${HOST}:${PORT}`);
+
+  await sequelize
+    .authenticate()
+    .then(async () => {
+      console.log(`connection success ${DATEBASE}`);
+    })
+    .catch((e) => {
+      console.log("error: ", e);
+    });
 });
