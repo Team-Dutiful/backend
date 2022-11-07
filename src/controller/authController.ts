9f855deb238f7ac8264b2f2cf55154b041deefad
dain7 <dainnnnnn96@gmail.com>
2022-11-07 12:35:40 +0900

    [feat] group controllers 생성

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import AuthService from "../services/authService";
+import AuthService from "@services/authService";
+
 const service = new AuthService();
 
 const login = async (req: Request, res: Response) => {
--- src/controller/groupsController.ts
+++ src/controller/groupsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import GroupService from "../services/groupsService";
+import GroupService from "@services/groupsService";
 const service = new GroupService();
 
 const createGroup = async (req: Request, res: Response) => {
@@ -13,11 +13,75 @@ const createGroup = async (req: Request, res: Response) => {
 
 const getGroup = async (req: Request, res: Response) => {
   try {
-    await service.getGroup();
-    return res.status(200).json({ status: 200 });
+    const groups = await service.getGroup();
+    return res.status(200).json({ status: 200, data : groups });
   } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
   }
 };
 
-export default { createGroup, getGroup };
+const updateGroup = async (req: Request, res: Response) => {
+  try {
+    const groupId = await service.updateGroup(req.params.groupId, req.body.name, req.body.color);
+    return res.status(200).json({ status: 200, id: groupId });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+const deleteGroup = async (req: Request, res: Response) => {
+  try {
+    const groupId = await service.deleteGroup(req.params.groupId);
+    return res.status(200).json({ status: 200, id: groupId });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+const banGroup = async (req: Request, res: Response) => {
+  try {
+    const groupId = await service.banGroup(req.params.groupId, req.body.user_id);
+    return res.status(200).json({ status: 200, id: groupId });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+const exitGroup = async (req: Request, res: Response) => {
+  try {
+    await service.exitGroup(req.params.groupId, req.body.user_id);
+    return res.status(200).json({ status: 200});
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+const changeLeader = async (req: Request, res: Response) => {
+  try {
+    const groupId = await service.changeLeader(req.params.groupId, req.body.user_id);
+    return res.status(200).json({ status: 200, id: groupId });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+const getGroupMembers = async (req: Request, res: Response) => {
+  try {
+    const groupId = await service.getGroupMembers(req.params.groupId);;
+    return res.status(200).json({ status: 200, id: groupId });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+const getScheduleByMembers = async (req: Request, res: Response) => {
+  try {
+    const groupId = await service.getScheduleByMembers(req.params.group_id);
+    return res.status(200).json({ status: 200, id: groupId });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+
+export default { createGroup, getGroup, updateGroup, deleteGroup, banGroup, exitGroup, changeLeader, getGroupMembers, getScheduleByMembers};
