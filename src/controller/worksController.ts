d4e6c79fb4014c6f2c69293e4a74a7e554550288
na_0_i <nayoi1oo5@gmail.com>
2022-11-09 20:01:00 +0900

    [feat] work create api 생성

--- src/controller/worksController.ts
+++ src/controller/worksController.ts
@@ -4,7 +4,14 @@ const service = new WorksService();
 
 const createWork = async (req: Request, res: Response) => {
   try {
-    await service.createWork();
+    await service.createWork(
+      req.body.name,
+      req.body.color,
+      req.body.start_time,
+      req.body.end_time,
+      req.body.work_type,
+      req.body.memo
+    );
     return res.status(200).json({ status: 200 });
   } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
--- src/services/worksService.ts
+++ src/services/worksService.ts
@@ -1,10 +1,48 @@
+import { Work } from "@db/models/work";
+import { User } from "@db/models/user";
+import { CalendarDate } from "@db/models/calendar-date";
+
 class WorksService {
   getWork = async () => {
     console.log("test");
   };
 
-  createWork = async () => {
-    console.log("test");
+  createWork = async (
+    name: string,
+    color: string,
+    start_time: Date,
+    end_time: Date,
+    work_type: string,
+    memo: string
+  ) => {
+    try {
+      const newUser = await User.create({
+        identification: "hello",
+        password: "1234",
+        name: "hi",
+        email: "tmax.com",
+      });
+      const newWork = await Work.create(
+        {
+          name: "name",
+          color: "#12345",
+          start_time: new Date(),
+          end_time: new Date(),
+          work_type: "DAY",
+          memo: "memo",
+          user_id: newUser.user_id,
+        },
+        {
+          include: [
+            {
+              model: User,
+            },
+          ],
+        }
+      );
+    } catch (error) {
+      throw error;
+    }
   };
 }
 
